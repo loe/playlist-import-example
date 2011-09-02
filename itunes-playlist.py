88f9d94b8b5918da80f73c2e7dfe989f9304c95f
W. Andrew Loe III <andrew@andrewloe.com>
2011-09-02 14:29:31 -0700

    Support importing a list of playlists.

--- itunes-playlist.py
+++ itunes-playlist.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-PLAYLIST_NAME="My Top Rated"
+PLAYLIST_NAMES=["My Top Rated"] # Supports a list
 PLAYLIST_DESCRIPTION="Imported from iTunes"
 
 import sys, logging
 from playlistcreator import PlaylistCreator
+from ScriptingBridge import *
+
 pc = PlaylistCreator()
 if not pc.authenticated:
   print 'You need to authenticate by running ./authenticate.py first'
@@ -12,14 +14,15 @@ if not pc.authenticated:
 
 logging.basicConfig(level=logging.WARNING)
 
-
-from ScriptingBridge import *
-itunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
-playlist = itunes.sources()[0].playlists().objectWithName_(PLAYLIST_NAME)
-itunes_tracks = playlist.tracks()
-rdio_tracks = []
-for track in itunes_tracks:
+for playlist_name in PLAYLIST_NAMES:
+  itunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
+  playlist = itunes.sources()[0].playlists().objectWithName_(playlist_name)
+  itunes_tracks = playlist.tracks()
+  rdio_tracks = []
+  for track in itunes_tracks:
     #logging.info("%s - %s" % (track.artist().encode("ascii", "replace"), track.name().encode("ascii", "replace")))
     rdio_tracks.append((track.artist(),track.name()))
 
-pc.make_playlist(PLAYLIST_NAME, PLAYLIST_DESCRIPTION, rdio_tracks)
+  if rdio_tracks:
+    pc.make_playlist(playlist_name, PLAYLIST_DESCRIPTION, rdio_tracks)
+
